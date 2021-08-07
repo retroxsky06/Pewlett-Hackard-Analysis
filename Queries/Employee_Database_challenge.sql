db2a1cef4ae9939d881a636021ba7d7a392aad75
Vanessa Aczon <aczon.vanessa@gmail.com>
2021-08-06 20:47:09 -0700

    Uploading Deliverable 2 Challenge

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -34,3 +34,27 @@ GROUP BY title
 ORDER BY COUNT(title)DESC;
 
 SELECT * FROM retiring_titles;
+
+-- DELIVERABLE 2: The Employees Eligible for the Mentorship Program
+-- Use the DISTINCT ON to retreive the 1st occurrence
+SELECT DISTINCT ON (emp.emp_no) emp.emp_no,
+	emp.first_name,
+	emp.last_name,
+	emp.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees AS emp
+INNER JOIN dept_emp AS de
+ON (emp.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON (emp.emp_no = ti.emp_no)
+WHERE (emp.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+		AND (de.to_date = '9999-01-01')
+ORDER BY emp.emp_no;
+
+SELECT * FROM mentorship_eligibility;
+
+SELECT COUNT(first_name)
+FROM mentorship_eligibility;
